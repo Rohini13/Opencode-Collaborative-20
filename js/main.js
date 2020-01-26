4a0587c9196414f7ae1918dddfaff86407512c23
Rohini Mishra <iit2018087@iiita.ac.in>
2020-01-27 03:28:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,13 +45,71 @@ $(window).scroll(function() {
         $('#back2Top').fadeOut();
     }
 });
+  
 $(document).ready(function() {
+  var participants;
+  var paticipantJson=data/participants.json";
+    
     $("#back2Top").click(function(event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
     });
 
+  $.getJSON(participantsJson, function(data) {
+        participants = data.participants;
+        console.log(participants);
+        $.each(participants, function(i, participant) {
+            if (/^ *$/.test(participant.imageurl) || participant.imageurl == "") {
+                participant.imageurl =
+                    "https://image.flaticon.com/icons/svg/1141/1141771.svg";
+            } else {
+                var url = participant.imageurl;
+               
+
+                try {
+                    var http = new XMLHttpRequest();
+                    http.open("HEAD", url, false);
+                    http.send();
+
+                    if (http.status == 404) {
+                        participant.imageurl =
+                            "https://image.flaticon.com/icons/svg/1141/1141771.svg";
+                      //Icon made by Freepik from www.flaticon.com *credits*
+                    }
+                } catch (err) {}
+            }
+            var participantDiv =
+                "<div class='col-lg-3 col-sm-6 text-center mb-4'>" +
+                "<div class='card participant-card'>" +
+                "<img class='card-img-top participant-img' src=" +
+                participant.imageurl +
+                " alt=''>" +
+                "<div class='card-body'>" +
+                "<h4 class='card-title'>" +
+                participant.name +
+                "</h4>" +
+                "<p class='card-text'>" +
+                participant.about +
+                "</p>" +
+                "</div>" +
+                "<div class='social-media-links'>" +
+                "<a href=" +
+                participant.facebook +
+                "><i class='fab fa-facebook-f'></i></a>" +
+                "<a href=" +
+                participant.github +
+                "><i class='fab fa-github'></i></a>" +
+                "<a href=" +
+                participant.twitter +
+                "><i class='fab fa-twitter'></i></a>" +
+                "</div>" +
+                "</div>" +
+                "</div>";
+
+            $("#mentors").append(participantDiv);
+        });
+    });
 
 
 });
